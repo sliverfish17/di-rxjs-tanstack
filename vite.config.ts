70aae6441a89efbcb8246c80ecc9e2199b5aa25c
sliverfish17 <a1708kolot@gmail.com>
2024-09-03 20:32:41 +0300

    more production like vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,16 +5,41 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite';
 
-export default defineConfig({
-  plugins: [TanStackRouterVite(), react()],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/setupTests.ts',
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
+export default defineConfig(() => {
+  return {
+    plugins: [
+      TanStackRouterVite(),
+      react(),
+    ],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
     },
-  },
+    build: {
+      target: 'esnext',
+      outDir: 'dist',
+      sourcemap: true, // sourcemaps to debug
+      minify: 'terser', // better minification
+      terserOptions: {
+        compress: {
+          drop_console: true, // removes log in prod
+        },
+      },
+      rollupOptions: {
+        output: {
+          manualChunks: (id) => {
+            if (id.includes('node_modules')) {
+              return 'vendor';
+            }
+          },
+        },
+      },
+    },
+    test: {
+      globals: true,
+      environment: 'jsdom',
+      setupFiles: './src/setupTests.ts',
+    },
+  };
 });
